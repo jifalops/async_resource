2be8679b75578774f9d27c83a1e0d71e108652b9
Jacob Phillips <jifalops@gmail.com>
2018-08-03 06:00:06 -0400

    Added session storage

--- lib/browser_resource.dart
+++ lib/browser_resource.dart
@@ -1,11 +1,12 @@
 library async_resource;
 
 import 'dart:async';
+import 'dart:html';
 import 'package:meta/meta.dart';
 import 'package:service_worker/worker.dart' as sw;
 import 'package:async_resource/async_resource.dart';
 
-/// A network resource fetched and cached by a service-worker in a browser
+/// A network resource fetched and cached by a service worker in a browser
 /// environment.
 class ServiceWorkerResource<T> extends NetworkResource<T> {
   ServiceWorkerResource(
@@ -58,9 +59,7 @@ class ServiceWorkerCacheEntry<T> extends LocalResource<T> {
     final resp = await response;
     final headers = resp?.headers;
     if (headers != null && headers.has('date')) {
-      try {
-        return DateTime.parse(headers['date']);
-      } catch (ignored) {}
+      return DateTime.tryParse(headers['date']);
     }
     return null;
   }
@@ -80,3 +79,54 @@ bool _isValid(sw.Response response) {
   if (response.type == 'error') return false;
   return true;
 }
+
+/// A single entry in a [window.localStorage] or [window.sessionStorage] map.
+class StorageEntry<T> extends LocalResource<T> {
+  StorageEntry(
+      {@required this.key,
+      this.type: StorageType.localStorage,
+      this.saveLastModified: false,
+      Parser parser})
+      : super(path: type.name, parser: parser);
+
+  final StorageType type;
+  final String key;
+
+  /// Create a duplicate storage entry when this entry is written.
+  final bool saveLastModified;
+
+  Map<String, String> get map => type == StorageType.localStorage
+      ? window.localStorage
+      : window.sessionStorage;
+
+  String get value => map[key];
+
+  @override
+  Future<bool> get exists async => map.containsKey(key);
+
+  @override
+  Future fetchContents() async => value;
+
+  @override
+  Future<DateTime> get lastModified async =>
+      saveLastModified ? DateTime.tryParse(map[key + '_modified']) : null;
+
+  @override
+  Future<void> write(contents) async {
+    map[key] = contents;
+    if (saveLastModified) {
+      map[key + '_modified'] = DateTime.now().toIso8601String();
+    }
+  }
+}
+
+/// See [window.localStorage] and [window.sessionStorage].
+class StorageType {
+  static const localStorage = StorageType._('localStorage');
+  static const sessionStorage = StorageType._('sessionStorage');
+  const StorageType._(this.name);
+  final String name;
+}
+
+
+
